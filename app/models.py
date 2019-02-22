3b4a40b1415860c634b0a95b70d9dd43595153bc
WEISHUNLIAO <weishun.liao@ucdconnct.ie>
2019-02-22 11:08:33 +0000

    add dynamic info class in model

--- app/models.py
+++ app/models.py
@@ -20,3 +20,18 @@ class Stations(db.Model):
         self.p_longitude = longitude
         self.bike_stands = bike_stands
         self.banking = banking
+
+
+class DynamicInfo(db.Model):
+    ID = db.Column(db.Integer(), primary_key=True)
+    last_update = db.Column(db.BigInteger(), primary_key=True)
+    available_bikes = db.Column(db.Integer())
+    available_bike_stands = db.Column(db.Integer())
+    status = db.Column(db.VARCHAR(255))
+
+    def __init__(self, ID, last_update, available_bikes, available_bike_stands, status):
+        self.ID = ID
+        self.last_update = last_update
+        self.available_bikes = available_bikes
+        self.available_bike_stands = available_bike_stands
+        self.status = status
