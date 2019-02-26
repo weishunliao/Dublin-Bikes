7092166a57ddee7734ed23cf00f12ceb2cbf91a5
WEISHUNLIAO <weishun.liao@ucdconnect.ie>
2019-02-26 11:47:57 +0000

    Added scrape_weather_crontab.py

---
+++ Web Scraping/scrape_weather_crontab.py
@@ -0,0 +1,43 @@
+import requests
+import time
+import json
+import mysql.connector
+
+get_url = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&units=metric&APPID=adfe6377cfc67c4eba8b0cf52ec30bbf'
+response = requests.get(get_url)
+response_json = response.json()
+response.close()
+getDataTime = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+dynamicData = open('/home/ubuntu/scrape/weather_data/' +
+                   getDataTime + '.json', 'w')
+json.dump(response.json(), dynamicData)
+dynamicData.close()
+
+conn = mysql.connector.connect(
+    host="dbbikes.coj48ycjuqgc.us-east-2.rds.amazonaws.com",
+    user="LFL_DBBIKES",
+    passwd="MYrds123",
+    database="dbbike")
+cursor = conn.cursor()
+
+sql_query = '''INSERT INTO weather(`temp`,`weather`,`w_description`,`visibility`,`wind_speed`,`wind_degree`,`datetime`,`day`,`week`) VALUES (%s, %s, %s, %s, %s, %s,%s,%s,%s)'''
+
+temp = response_json["main"]["temp"]
+weather = response_json["weather"][0]['main']
+description = response_json["weather"][0]["description"]
+visibility = response_json["visibility"]
+wind_speed = response_json["wind"]["speed"]
+wind_degree = response_json["wind"]["deg"]
+datetime = response_json["dt"]
+ltime = time.gmtime(datetime)
+day = time.strftime("%Y-%m-%d %H:%M:%S", ltime)
+week = ltime[6] + 1
+try:
+    cursor.execute(sql_query, (temp, weather, description,
+                               visibility, wind_speed, wind_degree, datetime, day, week))
+    conn.commit()
+except Exception as err:
+    log = open('/home/ubuntu/scrape/log/weather' + getDataTime, 'w')
+    conn.rollback()
+    # print(err)
+    log.close()
