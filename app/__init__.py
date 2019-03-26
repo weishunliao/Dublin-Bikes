e93651b460f6f225b53e487abdc75eff25e55ecc
WEISHUNLIAO <weishun.liao@ucdconnct.ie>
2019-03-26 15:21:00 +0000

    remove  unnecessary config files

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,instance_relative_config=True)
 
-app.config.from_object('config.default')
+# app.config.from_object('config.default')
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
